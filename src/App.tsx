b90a48e874ce79faa73bbf07908f9bc53419b9f1
jbyomuhangi <jbyomuhangi@gmail.com>
2023-05-07 22:55:54 +0800

    Connected the MUI ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 
+import theme from "./theme";
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>App</div>;
+    </ThemeProvider>
+  );
 };
 
 export default App;
